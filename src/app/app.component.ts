cd3665ddce49b7bfd4802dba30e45499c1d06aae
Shawn Dougherty <sdougherty@milestechnologies.com>
2019-01-02 13:49:49 -0500

    Initialized Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  isNavbarCollapsed = false;
+
   title = 'BattleForge';
+  battleForgeArray = [];
+
+  addBF(value) {
+    this.battleForgeArray.push(value)
+    console.log(value)
+  }
 }
